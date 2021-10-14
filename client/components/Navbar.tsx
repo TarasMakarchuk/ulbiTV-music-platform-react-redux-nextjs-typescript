5b35b677cc3f14346c76a8a767a995069d9b837b
TarasMakarchuk <2278452@gmail.com>
2021-10-14 12:35:09 +0300

    add client side

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import Box from '@material-ui/core/Box';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import {AppBar} from "@material-ui/core";
+import { useTheme } from '@material-ui/core/styles';
+import {useRouter} from "next/router";
+
+const menuItems = [
+  {text: 'Main page', href: '/'},
+  {text: 'Tracks list', href: '/tracks'},
+  {text: 'Albums list', href: '/albums'},
+];
+
+export default function Navbar() {
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const router = useRouter();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', backgroundColor: 'blue', color: 'white' }}>
+      <CssBaseline />
+      {/*<AppBar position="fixed" open={open}>*/}
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      {/*</AppBar>*/}
+      <Drawer
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 240,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        {/*<DrawerHeader>*/}
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        {/*</DrawerHeader>*/}
+        <Divider />
+
+        <List>
+          {menuItems.map(({text, href}, index) => (
+            <ListItem button key={href} onClick={() => router.push(href)}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
